61b3155c9986929006dd5731861fe7c1a3cb228f
goss.beta <goss.beta@gmail.com>
2023-03-09 00:26:04 +0900

    chore(gradle): refactoring versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
-val ktorVersion = "2.2.3"
-val kotlinVersion = "1.8.10"
-val logbackVersion = "1.2.11"
-val postgresVersion = "42.5.1"
-val h2Version = "2.1.214"
-val exposedVersion = "0.41.1"
-val prometeusVersion = "1.6.3"
-val koinVersion = "3.3.3"
-val koinKtorVersion = "3.3.1"
-val hopliteVersion = "2.7.2"
-val hikariCPVersion = "5.0.1"
+object versions {
+    const val ktor = "2.2.3"
+    const val kotlin = "1.8.10"
+    const val logback = "1.2.11"
+    const val postgres = "42.5.1"
+    const val h2 = "2.1.214"
+    const val exposed = "0.41.1"
+    const val prometheus = "1.6.3"
+    const val koin = "3.3.3"
+    const val koinKtor = "3.3.1"
+    const val hoplite = "2.7.2"
+    const val hikariCP = "5.0.1"
+}
 
 plugins {
     kotlin("jvm") version "1.8.10"
@@ -31,33 +33,33 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:${versions.ktor}")
+    implementation("io.ktor:ktor-server-call-logging-jvm:${versions.ktor}")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:${versions.ktor}")
+    implementation("io.ktor:ktor-server-core-jvm:${versions.ktor}")
+    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:${versions.ktor}")
+    implementation("io.ktor:ktor-server-netty-jvm:${versions.ktor}")
+    testImplementation("io.ktor:ktor-server-tests-jvm:${versions.ktor}")
 
-    implementation("io.insert-koin:koin-core:$koinVersion")
-    implementation("io.insert-koin:koin-ktor:$koinKtorVersion")
-    implementation("io.insert-koin:koin-logger-slf4j:$koinKtorVersion")
-    testImplementation("io.insert-koin:koin-test:$koinVersion")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    implementation("io.insert-koin:koin-core:${versions.koin}")
+    implementation("io.insert-koin:koin-ktor:${versions.koinKtor}")
+    implementation("io.insert-koin:koin-logger-slf4j:${versions.koinKtor}")
+    testImplementation("io.insert-koin:koin-test:${versions.koin}")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:${versions.kotlin}")
 
-    implementation("org.postgresql:postgresql:$postgresVersion")
-    implementation("com.h2database:h2:$h2Version")
+    implementation("org.postgresql:postgresql:${versions.postgres}")
+    implementation("com.h2database:h2:${versions.h2}")
 
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    implementation("com.zaxxer:HikariCP:$hikariCPVersion")
+    implementation("org.jetbrains.exposed:exposed-core:${versions.exposed}")
+    implementation("org.jetbrains.exposed:exposed-jdbc:${versions.exposed}")
+    implementation("com.zaxxer:HikariCP:${versions.hikariCP}")
 
-    implementation("io.micrometer:micrometer-registry-prometheus:$prometeusVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:${versions.prometheus}")
 
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("ch.qos.logback:logback-classic:${versions.logback}")
 
-    implementation("com.sksamuel.hoplite:hoplite-core:$hopliteVersion")
-    implementation("com.sksamuel.hoplite:hoplite-yaml:$hopliteVersion")
+    implementation("com.sksamuel.hoplite:hoplite-core:${versions.hoplite}")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:${versions.hoplite}")
 }
 
 tasks {
