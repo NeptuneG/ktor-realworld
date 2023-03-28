f278036d55f69a99dda3d9cc5d99687352244061
goss.beta <goss.beta@gmail.com>
2023-03-28 22:02:48 +0900

    feat(exposed): create extension runTxCatching

--- src/main/kotlin/com/neptuneg/adaptor/database/gateway/repository/FollowingRepositoryImpl.kt
+++ src/main/kotlin/com/neptuneg/adaptor/database/gateway/repository/FollowingRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.neptuneg.adaptor.database.gateway.repository
 
 import com.neptuneg.adaptor.database.gateway.entity.FollowingEntity
+import com.neptuneg.adaptor.database.gateway.extension.runTxCatching
 import com.neptuneg.adaptor.database.gateway.table.FollowingsTable
 import com.neptuneg.domain.entity.Following
 import com.neptuneg.domain.logic.FollowingRepository
@@ -11,29 +12,23 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 class FollowingRepositoryImpl: FollowingRepository {
     override fun isExisting(following: Following): Result<Boolean> {
-        return runCatching {
-            transaction {
-                FollowingsTable.select { by(following) }.count() != 0L
-            }
+        return runTxCatching {
+            FollowingsTable.select { by(following) }.count() != 0L
         }
     }
 
     override fun create(following: Following): Result<Unit> {
-        return runCatching {
-            transaction {
-                FollowingEntity.new {
-                    followerId = following.followerId
-                    followeeId = following.followeeId
-                }
+        return runTxCatching {
+            FollowingEntity.new {
+                followerId = following.followerId
+                followeeId = following.followeeId
             }
         }
     }
 
     override fun delete(following: Following): Result<Unit> {
-        return runCatching {
-            transaction {
-                FollowingEntity.find { by(following) }.first().delete()
-            }
+        return runTxCatching {
+            FollowingEntity.find { by(following) }.first().delete()
         }
     }
 
